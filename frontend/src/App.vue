2be2c2084325ec68fd994fc9f11676ba9a8a46b9
markedrianst <markedriantalavera070@gmail.com>
2026-02-06 17:03:32 +0800

    Project Initialization

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+
+<template>
+<h1 class="text-4xl font-bold text-blue-600">
+  Tailwind Vue Ready 🚀
+</h1>
+</template>
---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './styles.css'
+
+const app = createApp(App)
+
+app.config.devtools = false
+
+app.mount('#app')
